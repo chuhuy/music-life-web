42b1eae7becfffb9e55d9bf360a0f5ba9a579855
chuhuy <45164683+chuhuy@users.noreply.github.com>
2020-12-01 23:42:47 +0700

    Update constants

--- src/app/shared/components/header/index.tsx
+++ src/app/shared/components/header/index.tsx
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { RefObject, useRef, useState } from 'react';
 import './styles.css';
 import { Row } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faRoad } from '@fortawesome/free-solid-svg-icons';
+import { useWindowDimensions } from './../../../hooks/useWindowDimensions';
+import { tabletWidth } from './../../constants/devices';
+import { Formik, Field } from 'formik';
+import { SignInForm } from '../../../models/form/signIn';
 
 interface Props {}
 
 const Header: React.FunctionComponent<Props> = (props: Props) => {
     
+    const { width } = useWindowDimensions();
+
     const handleOpenSetting = () => {
         alert('Setting')
-    }
+    };
+    const initialValue: any = {
+        search: '',
+    };
+    
+    const [inputRefs] = useState<{
+		[key: string]: RefObject<HTMLInputElement>;
+	}>({
+        search: useRef<HTMLInputElement>(null)
+    });
+    
+    const handleSearch = (values:  any) => {
+        console.log(values);
+    };
 
     return (
         <>
-            <Row id="header-container">
-                <div id="header-search"></div>
+            <Row id="header-container" style={width < tabletWidth ? {marginBottom: '40px'} : {}}>
+                <Formik 
+                    initialValues={initialValue}
+                    onSubmit={(values) => {handleSearch(values);}}>
+                    {({handleSubmit, values}) => (<>
+                        <div id="header-search">
+                            <Field
+                                innerRef={inputRefs.search}
+                                component="input"
+                                className="search-input"
+                                placeholder="Type here to search"
+                                row={1}
+                                id="inputSearch"
+                                name="search"
+                            />
+                        </div>
+                    </>)}
+                </Formik>
                 <div id="header-setting">
                     <button className="player-icon-button" onClick={() => handleOpenSetting()}>
                         <FontAwesomeIcon icon={faCog} color="#FFF" size="1x" />
---
+++ src/app/shared/constants/devices.ts
@@ -0,0 +1,5 @@
+export const phoneWidth = 357;
+export const tabletWidth = 768;
+export const smallPCWidth = 768;
+export const mediumPCWidth = 992;
+export const largePCWidth = 1200;
